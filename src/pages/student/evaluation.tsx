495a116c76b08b554876a66887ccdd66fe604d48
Prajwal <prajwify@gmail.com>
2023-05-13 17:20:06 +0530

    centered evaluation button

--- src/pages/student/evaluation.tsx
+++ src/pages/student/evaluation.tsx
@@ -16,8 +16,8 @@ export default function evaluation() {
             <div className='py-2 mx-10 flex flex-col gap-2'>
                 <p className={`${inter.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae ipsum est. Quisque nec diam et turpis elementum interdum sed sed velit. In ut tortor egestas, blandit lectus in, cursus justo. Aenean ultricies, nibh nec aliquam ultrices, massa tortor blandit orci, ac vehicula ipsum enim eget leo. Duis vel dictum eros. Mauris pellentesque purus in erat imperdiet ultricies. Ut dui elit, bibendum ac egestas a, iaculis dignissim orci. Cras molestie vulputate risus at ornare. Duis faucibus metus vitae nulla consectetur viverra. Sed egestas non nibh ac congue. Proin eget lobortis elit.</p>
                 <textarea rows={6} className='w-full p-2 border-2 border-black' />
-                <div className='flex justify-end'>
-                    <button className='p-3 bg-yellow-400 hover:bg-yellow-500'>Evaluate</button>
+                <div className='flex justify-center'>
+                    <button className='p-3 px-10 bg-yellow-400 hover:bg-yellow-500'>Evaluate</button>
                 </div>
             </div>
         </div>
